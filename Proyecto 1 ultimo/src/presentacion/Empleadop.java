20e1c0318ceb509e576bc5e1abb1ae2ecd2f4a91
Mparrao3 <111020027+Mparrao3@users.noreply.github.com>
2023-05-06 22:11:50 -0500

    Update Empleadop.java

--- Proyecto 1 ultimo/src/presentacion/Empleadop.java
+++ Proyecto 1 ultimo/src/presentacion/Empleadop.java
@@ -11,7 +11,7 @@ public class Empleadop extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Launch the application...
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
